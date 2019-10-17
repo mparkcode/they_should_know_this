2a079870e6b5592326a333df86b9ca60d90c9065
Michael Park <mparkcode@gmail.com>
2019-10-17 20:07:40 +0000

    update

--- run.py
+++ run.py
@@ -17,19 +17,19 @@ def about():
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
-    
+
 @app.route("/about/<member_name>")
 def about_member(member_name):
     member = {}
-    
+
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-                
+
     return render_template("member.html", member=member)
-    
+
 
 
 @app.route('/contact', methods=["GET", "POST"])
@@ -44,6 +44,6 @@ def careers():
     return render_template("careers.html", page_title="Careers")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',
-    port=int('5000'),
+    app.run(host=os.environ.get('IP'),
+    port=int(os.environ.get('PORT')),
     debug=True)
